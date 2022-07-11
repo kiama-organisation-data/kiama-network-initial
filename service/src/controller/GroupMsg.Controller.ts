5e7eee9428492c4fe5643092118f95d5d212f2ff
Xlaez <utibeabasiekong2@gmail.com>
2022-07-11 09:46:14 +0100

    added messaging feature for groups

--- service/src/controller/GroupMsg.Controller.ts
+++ service/src/controller/GroupMsg.Controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { groupModel, IGroup } from "../model/Messages.Model";
+import {
+  groupModel,
+  groupMsgModel,
+  IGrmsg,
+  IGroup,
+} from "../model/Messages.Model";
 import Users from "../model/UsersAuth.Model";
 
 /**
@@ -10,10 +15,21 @@ import Users from "../model/UsersAuth.Model";
  * @function editGroupPhoto will edit the group photo accepting only jpg, jpeg and png.
  * @function deleteGroupPhoto will set the publicId and url of group image to null
  * @param isAdmin remember to set this to a middleware in the future
+ * @param createGroup admin should be changed to work with more than one id
+ * @function removeMember in the future should also remove the groups id from the group
+ * param in user model
+ * @function deleteGroup in the future should remove the groups id from the user model
+ * @function sendMessage in the future a timeStamp will be used to calculate seconds before
+ * not allowing for deleting anymore
  */
 
 class GroupController {
   constructor() {}
+
+  /**
+   * Up until next comment at line 200+, here only handles group functionalities
+   * and not messaging in group
+   */
 
   createGroup = async (req: Request, res: Response) => {
     //id is the id of the group creator
@@ -27,12 +43,12 @@ class GroupController {
     }
 
     const { id } = req.params;
-    const user = await Users.findById(id).select(["username", "_id"]);
+    let user = await Users.findById(id).select(["username", "_id", "groups"]);
 
     if (!user)
       return res.status(404).json({ success: "fail", msg: "user not found" });
 
-    const group = await groupModel.create({
+    const group: IGroup = await groupModel.create({
       ...req.body,
       members: [id],
       admin: {
@@ -44,12 +60,15 @@ class GroupController {
         publicId: null,
       },
       isImage: req.file ? true : false,
+      size: 1,
     });
 
+    user.groups.push(group._id);
+    user = await user.save();
     if (!group) {
       res.status(400).json({ success: "fail" });
     } else {
-      res.status(201).json({ success: "true", data: group });
+      res.status(201).json({ success: "true", data: group, user });
     }
   };
 
@@ -158,13 +177,16 @@ class GroupController {
       if (group.admin.id.toString() !== id)
         return res.status(400).json({ success: "fail", msg: "not permitted" });
 
-      const user = await Users.findById(req.body.member);
+      let user = await Users.findById(req.body.member);
 
       if (!user)
         return res.status(404).json({ success: "fail", msg: "user not found" });
 
+      user.groups.push(group._id);
       group.members.push(req.body.member); //req.body.member is user id;
+      group.size = group.size + 1;
       group = await group.save();
+      user = await user.save();
       res.status(200).json({ success: "true", data: group });
     }
   };
@@ -194,20 +216,150 @@ class GroupController {
       //@ts-expect-error
       const deleted = await group.removeMember(memberToBeDeleted);
       if (deleted) {
-        res
-          .status(200)
-          .json({ success: "true", data: group, memberToBeDeleted });
+        res.status(200).json({ success: "true", data: group });
       } else {
         res.status(400).send({ success: "fail" });
       }
     }
   };
-}
 
-/**
- * users can send request
- * admin can remove members
- * mebers can exit group
- */
+  deleteGroup = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (!id)
+      return res
+        .status(400)
+        .json({ success: "fail", msg: "please provide an id" });
+
+    await groupModel.findByIdAndDelete(id);
+    try {
+      res.status(200).json({ success: "true" });
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  };
+
+  exitGroup = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    if (!id) {
+      return res
+        .status(400)
+        .json({ success: "fail", msg: "please provide the user id" });
+    }
+
+    let user = await Users.findById(id).select("groups");
+
+    let group = await groupModel.findById(req.body.id).select("removeMember");
+
+    if (!user || !group) {
+      res.status(404).json({ success: "fail", msg: "user can't be found" });
+    } else {
+      const result = group.removeMember(id);
+      if (!result) return res.status(400).json({ success: "fail" });
+      res.status(200).json({ success: "true", data: group });
+    }
+  };
+
+  /**
+   * From here down begins the implementation of all the messaging functionalities
+   */
+
+  sendMessage = async (req: Request, res: Response) => {
+    let audio: object = {};
+    let image: object = {};
+    let text: string = "";
+    let msgFormat: string = "";
+
+    const audioMimeType = ["audio/mp3", "audio/wmp", "audio/mpeg"];
+    const imageMimeType = ["image/jpeg", "image/pdf", "image/gif"];
+
+    if (req.file) {
+      if (audioMimeType.includes(req.file.mimetype)) {
+        audio = {
+          publicId: "nothing yet", //Note: would be implemented later with cloudinary
+          url: req.file.path,
+        };
+        msgFormat = "audio";
+      } else if (imageMimeType.includes(req.file.mimetype)) {
+        image = {
+          publicId: "nothing yet", //Note: would be implemented later with cloudinary
+          url: req.file.path,
+        };
+        msgFormat = "image";
+      } else {
+        res.status(400).json({ msg: "not a valid file type", success: "fail" });
+      }
+    } else {
+      if (req.body) {
+        text = req.body.text;
+        msgFormat = "text";
+      }
+    }
+    const newMsg: IGrmsg = await groupMsgModel.create({
+      message: {
+        text,
+        audio,
+        image,
+      },
+      sender: req.body.from,
+      messageFormat: msgFormat,
+      groupId: req.params.groupId,
+    });
+    if (!newMsg)
+      return res
+        .status(401)
+        .json({ success: "fail", msg: "couldn't send message" });
+
+    res.status(201).json({ success: "true", data: newMsg });
+  };
+
+  getMessages = async (req: Request, res: Response) => {
+    const { groupId } = req.params;
+
+    if (!groupId)
+      return res
+        .status(400)
+        .json({ success: "fail", msg: "please provide an id" });
+
+    const messages = await groupMsgModel.find({ groupId });
+
+    if (!messages) {
+      res.status(404).json({ success: "fail", msg: "no messages yet" });
+    } else {
+      res.status(200).json({ success: "fail", data: messages });
+    }
+  };
+
+  deleteMessage = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (!id)
+      return res
+        .status(400)
+        .json({ success: "fail", msg: "please provide an id" });
+
+    await groupMsgModel.findByIdAndDelete(id);
+    res.status(200).json({ success: "true" });
+  };
+
+  markSeen = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    if (!id)
+      return res
+        .status(400)
+        .json({ success: "fail", msg: "please provide an id" });
+
+    let message = await groupMsgModel.findById(id).select("seen");
+
+    if (!message) {
+      res.status(404).json({ success: "fail", msg: "cannot find message" });
+    } else {
+      //@ts-ignore
+      message?.seen = true;
+      message = await message.save();
+      res.status(200).json({ success: "true", data: message });
+    }
+  };
+}
 
 export default new GroupController();
