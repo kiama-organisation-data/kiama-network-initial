ecb1620e0a4ddf970ded99b46cf7545f4f5ba956
Andry Rasolofomanana <andry.rasolofomananaa@gmail.com>
2022-07-23 14:51:50 +0300

    feat: initialise collaborator model

---
+++ service/src/model/Collaborators.Model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+const shema: any = mongoose.Schema
+
+export interface ICollaborator extends mongoose.Document {
+    collaborator: string;
+    project: string;
+    role: string;
+    task: Array<string>;
+}
+
+const collaboratorsShema = new shema({
+    colaborators: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project"
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    task: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task"
+    }]
+}, { _id: true, timestamps: true })
+
+const Collaborators = mongoose.model<ICollaborator>('Collaborator', collaboratorsShema)
+
+export default Collaborators
