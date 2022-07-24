77a49129cb8c83171ab0c8cee805b55987b2d5f4
Andry Rasolofomanana <andry.rasolofomananaa@gmail.com>
2022-07-24 22:18:22 +0300

    feat: initialise call all router router

--- service/src/route/Index.ts
+++ service/src/route/Index.ts
@@ -8,6 +8,35 @@ import PostsRouter from "./Posts.Router";
 import RoleRouter from "./Role.Router";
 import UsersAuthRouter from "./UsersAuth.Router";
 import BugsRouter from "./BugsReport.Router";
+import ChallengesRouter from "./Challenges.Router";
+import EventsRouter from "./Events.Router";
+import CollaboratorsRouter from "./Collaborators.Router";
+import CommentsRouter from "./Comments.Router";
+import CountriesRouter from "./Countries.Router";
+import FavoritesRouter from "./Favorites.Router";
+import FriendReqsRouter from "./FriendReqs.Router";
+import HistorysRouter from "./Historys.Router";
+import NotificationsRouter from "./Notifications.Router";
+import LanguagesRouter from "./Languages.Router";
+import MedalsRouter from "./Medals.Router";
+import NewsRouter from "./News.Router";
+import OrientationsRouter from "./Orientations.Router";
+import ProfilesRouter from "./Profiles.Router";
+import ProjectsRouter from "./Projects.Router";
+import ReportsRouter from "./Reports.Router";
+import SessionsRouter from "./Sessions.Router";
+import SettingsRouter from "./Settings.Router";
+import StatisticsRouter from "./Statistics.Router";
+import TasksRouter from "./Tasks.Router";
+import VotesRouter from "./Votes.Router";
+
+// category
+import NewsCatRouter from "./category/News.Router";
+import ChallengesCatRouter from "./category/Challenges.Router";
+import EventsCatRouter from "./category/Events.Router";
+
+
+
 
 const router: Router = Router();
 
@@ -20,6 +49,32 @@ function rootRouter() {
   router.use("/post", validationToken.TokenValidation, PostsRouter);
   router.use("/channel", validationToken.TokenValidation, ChannelsRouter);
   router.use("/bug", validationToken.TokenValidation, BugsRouter);
+  router.use("/challenge", validationToken.TokenValidation, ChallengesRouter);
+  router.use("/event", validationToken.TokenValidation, EventsRouter);
+  router.use("/collaborator", validationToken.TokenValidation, CollaboratorsRouter);
+  router.use("/comment", validationToken.TokenValidation, CommentsRouter);
+  router.use("/country", validationToken.TokenValidation, CountriesRouter);
+  router.use("/favorite", validationToken.TokenValidation, FavoritesRouter);
+  router.use("/friendreq", validationToken.TokenValidation, FriendReqsRouter);
+  router.use("/history", validationToken.TokenValidation, HistorysRouter);
+  router.use("/notification", validationToken.TokenValidation, NotificationsRouter);
+  router.use("/language", validationToken.TokenValidation, LanguagesRouter);
+  router.use("/medal", validationToken.TokenValidation, MedalsRouter);
+  router.use("/news", validationToken.TokenValidation, NewsRouter);
+  router.use("/orientation", validationToken.TokenValidation, OrientationsRouter);
+  router.use("/profile", validationToken.TokenValidation, ProfilesRouter);
+  router.use("/project", validationToken.TokenValidation, ProjectsRouter);
+  router.use("/report", validationToken.TokenValidation, ReportsRouter);
+  router.use("/session", validationToken.TokenValidation, SessionsRouter);
+  router.use("/setting", validationToken.TokenValidation, SettingsRouter);
+  router.use("/statistic", validationToken.TokenValidation, StatisticsRouter);
+  router.use("/task", validationToken.TokenValidation, TasksRouter);
+  router.use("/vote", validationToken.TokenValidation, VotesRouter);
+
+  // category
+  router.use("/news/cat", validationToken.TokenValidation, NewsCatRouter);
+  router.use("/challenge/cat", validationToken.TokenValidation, ChallengesCatRouter);
+  router.use("/event/cat", validationToken.TokenValidation, EventsCatRouter);
 
   return router;
 }
