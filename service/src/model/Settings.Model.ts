2ea7d759adab15742918b5b56ec1d32c90fc2845
Andry Rasolofomanana <andry.rasolofomananaa@gmail.com>
2022-07-24 22:05:07 +0300

    feat: initialise settings, stat model

--- service/src/model/Settings.Model.ts
+++ service/src/model/Settings.Model.ts
@@ -3,11 +3,14 @@ import mongoose from "mongoose"
 const shema: any = mongoose.Schema
 
 export interface ISetting extends mongoose.Document {
-    
+    name: string
 }
 
 const settingsShema = new shema({
-
+    name: {
+        type: String,
+        required: true
+    }
 }, { _id: true, timestamps: true })
 
 const Settings = mongoose.model<ISetting>('Setting', settingsShema)
--- service/src/model/Statistics.Model.ts
+++ service/src/model/Statistics.Model.ts
@@ -3,11 +3,14 @@ import mongoose from "mongoose"
 const shema: any = mongoose.Schema
 
 export interface IStatistic extends mongoose.Document {
-
+    name: string
 }
 
 const statisticsShema = new shema({
-
+    name: {
+        type: String,
+        required: true
+    }
 }, { _id: true, timestamps: true })
 
 const Statistics = mongoose.model<IStatistic>('Statistic', statisticsShema)
