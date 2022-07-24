9669e6414afc86035f10ced31f24ed7c0189cae8
Andry Rasolofomanana <andry.rasolofomananaa@gmail.com>
2022-07-24 22:12:12 +0300

    feat: initialise challengePost router

---
+++ service/src/route/ChallengePosts.Router.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import challengepostsController from "../controller/ChallengePosts.Controller";
+import validationToken from "../libs/verifyToken";
+
+class ChallengePostsRouter {
+    router: Router;
+
+    constructor() {
+        this.router = Router();
+        this.routes();
+    }
+    /**
+     * @description: This method is used to define the routes of the ChallengePostsRouter
+     * @function routes "/:id" gets an id as a param used for fetching, deleting and editing
+     */
+    routes() {
+        this.router
+            .route("/")
+            .post(challengepostsController.create)
+            .get(challengepostsController.getAll);
+
+        this.router
+            .route("/:id")
+            .delete(challengepostsController.deleteOne)
+            .get(challengepostsController.getOne)
+            .patch(challengepostsController.update);
+    }
+}
+
+export default new ChallengePostsRouter().router;
