0fe0a16c1d5689a9d4bba2b4596c58243f695357
Andry Rasolofomanana <andry.rasolofomananaa@gmail.com>
2022-07-24 18:10:59 +0300

    feat: add bug report router

--- service/src/route/Index.ts
+++ service/src/route/Index.ts
@@ -7,17 +7,19 @@ import PostsPagesRouter from "./Posts.Pages.Router";
 import PostsRouter from "./Posts.Router";
 import RoleRouter from "./Role.Router";
 import UsersAuthRouter from "./UsersAuth.Router";
+import BugsRouter from "./BugsReport.Router";
 
 const router: Router = Router();
 
 function rootRouter() {
   router.use("/user", UsersAuthRouter);
-  router.use("/role", RoleRouter);
+  router.use("/role", validationToken.TokenValidation, RoleRouter);
   router.use("/page", validationToken.TokenValidation, PagesRouter);
   router.use("/msg", validationToken.TokenValidation, MessagesRouter);
   router.use("/pages/post", validationToken.TokenValidation, PostsPagesRouter);
   router.use("/post", validationToken.TokenValidation, PostsRouter);
   router.use("/channel", validationToken.TokenValidation, ChannelsRouter);
+  router.use("/bug", validationToken.TokenValidation, BugsRouter);
 
   return router;
 }
