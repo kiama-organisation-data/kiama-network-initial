f4265d698b12d89a75dbbf1f114d24b3b2c4c4e6
Andry Rasolofomanana <andry.rasolofomananaa@gmail.com>
2022-07-24 22:16:17 +0300

    feat: initialise stat, settings router

---
+++ service/src/route/Statistics.Router.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import statisticsController from "../controller/Statistics.Controller";
+import validationToken from "../libs/verifyToken";
+
+class StatisticsRouter {
+    router: Router;
+
+    constructor() {
+        this.router = Router();
+        this.routes();
+    }
+    /**
+     * @description: This method is used to define the routes of the StatisticsRouter
+     * @function routes "/:id" gets an id as a param used for fetching, deleting and editing
+     */
+    routes() {
+        this.router
+            .route("/")
+            .post(statisticsController.create)
+            .get(statisticsController.getAll);
+
+        this.router
+            .route("/:id")
+            .delete(statisticsController.deleteOne)
+            .get(statisticsController.getOne)
+            .patch(statisticsController.update);
+    }
+}
+
+export default new StatisticsRouter().router;
