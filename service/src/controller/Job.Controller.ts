2d4c9e597fcab565de81b64c46314db02644165f
Xlaez <utibeabasiekong2@gmail.com>
2022-07-26 20:56:17 +0100

    finished basic job portal arrangement

---
+++ service/src/controller/Job.Controller.ts
@@ -0,0 +1,289 @@
+import { Request, Response } from "express";
+import { deleteFromCloud, uploadToCloud } from "../libs/cloudinary";
+import sortData from "../middleware/utils";
+import { IJobPortal, jobModel, jobPortalModel } from "../model/Job.Model";
+import AppResponse from "../services/index";
+
+class JobCntrl {
+  constructor() {}
+
+  createJobPosting = async (req: Request, res: Response) => {
+    //@ts-ignore
+    const { body, user, file } = req;
+    let doc: object = {};
+
+    if (file) {
+      const { secure_url, public_id } = await uploadToCloud(file.path);
+      doc = { url: secure_url, publicId: public_id };
+    }
+
+    const job = await jobModel.create({
+      ...body,
+      poster: user,
+      portal: req.query.portal,
+      file: { ...doc, fileType: file?.mimetype },
+    });
+
+    try {
+      AppResponse.created(res, job);
+    } catch (error) {
+      AppResponse.fail(res, error);
+    }
+  };
+
+  deleteJobPost = async (req: Request, res: Response) => {
+    const jobPost = await jobModel.findById(req.params.postId);
+
+    if (!jobPost) {
+      return AppResponse.notFound(res);
+    }
+
+    //@ts-ignore
+    const publicId = jobPost.file.url;
+    let result: any;
+
+    if (publicId) {
+      result = await deleteFromCloud(publicId);
+    }
+
+    await jobModel.findByIdAndDelete(req.params.postId);
+
+    try {
+      AppResponse.success(res, result);
+    } catch (e) {
+      AppResponse.fail(res, e);
+    }
+  };
+
+  getJobPost = async (req: Request, res: Response) => {
+    const job = await jobModel.findById(req.params.postId).lean();
+
+    if (!job) {
+      AppResponse.notFound(res);
+    } else {
+      AppResponse.success(res, job);
+    }
+  };
+
+  getAllJobPost = (req: Request, res: Response) => {
+    let query;
+    jobModel
+      .find(query || {})
+      .then((jobPosting) => {
+        const {
+          search = "",
+          perPage = 10,
+          page = 1,
+          sortBy = "createdAt",
+          sortDesc = false,
+          jobPortalId = "",
+          select = "all",
+        } = req.query;
+
+        const querylowerd = search.toLowerCase();
+
+        const filteredData = jobPosting.filter((item) => {
+          return (
+            // search
+            item.poster.toString().includes(querylowerd) &&
+            // Filter
+            item.poster.toString() ===
+              (jobPortalId.toString() || item.poster.toString())
+          );
+        });
+
+        const sortedData = filteredData.sort(sortData.sortCompare(sortBy));
+        if (sortDesc === "true") {
+          sortedData.reverse();
+        }
+
+        const dataFinal = sortData.selectFields(sortedData, select);
+        AppResponse.success(
+          res,
+          sortData.paginateArray(dataFinal, perPage, page),
+          filteredData.length
+        );
+      })
+      .catch((e) => {
+        AppResponse.fail(res, e);
+      });
+  };
+
+  createPortal = async (req: Request, res: Response) => {
+    //@ts-expect-error
+    const { user } = req;
+
+    // @ts-expect-error
+    const upload = await uploadToCloud(req.file?.path);
+
+    try {
+      const portal: IJobPortal = await jobPortalModel.create({
+        ...req.body,
+        admins: [user],
+        coverPhoto: {
+          url: upload.secure_url,
+          publicId: upload.public_id,
+        },
+      });
+
+      AppResponse.created(res, portal);
+    } catch (e) {
+      AppResponse.fail(res, e);
+    }
+  };
+
+  editPortal = async (req: Request, res: Response) => {
+    //@ts-expect-error
+    const { body, user } = req;
+
+    const portal = await jobPortalModel.findByIdAndUpdate(
+      req.query.portal,
+      body,
+      { new: true }
+    );
+
+    if (!portal) {
+      return AppResponse.notFound(res, "portal not found");
+    }
+
+    try {
+      AppResponse.success(res, portal);
+    } catch (e) {
+      AppResponse.fail(res, e);
+    }
+  };
+
+  updatePortalCoverPhoto = async (req: Request, res: Response) => {
+    //@ts-expect-error
+    const { file, user } = req;
+    let procceed = false;
+
+    if (!file) {
+      return AppResponse.fail(res, "provide an image for upload");
+    }
+
+    try {
+      let portal = await jobPortalModel
+        .findById(req.params.portalId)
+        .select(["coverPhoto", "admins"]);
+
+      portal?.admins.map((admin: any) => {
+        if (user.toString() === admin.toString()) {
+          procceed = true;
+        }
+      });
+
+      if (!procceed) return AppResponse.notPermitted(res, "not an admin");
+
+      //@ts-expect-error
+      await deleteFromCloud(portal?.coverPhoto.url);
+      const upload = await uploadToCloud(file.path);
+
+      if (!upload) {
+        AppResponse.fail(res, "could not update cover photo");
+      }
+
+      await jobPortalModel.findByIdAndUpdate(
+        req.params.portalId,
+        { coverPhoto: { url: upload.secure_url, publicId: upload.public_id } },
+        { new: true }
+      );
+
+      AppResponse.success(res, portal);
+    } catch (e) {
+      AppResponse.fail(res, e);
+    }
+  };
+
+  deletePortal = async (req: Request, res: Response) => {
+    const { portalId } = req.params;
+    //@ts-expect-error
+    const { user } = req;
+    let proceed = false;
+
+    const portal = await jobPortalModel
+      .findById(portalId)
+      .select(["coverPhoto", "admins"])
+      .lean();
+
+    if (!portal) {
+      return AppResponse.notFound(res);
+    }
+
+    portal?.admins.map((admin: any) => {
+      if (user.toString() === admin.toString()) {
+        proceed = true;
+      }
+    });
+
+    try {
+      if (!proceed) return AppResponse.notPermitted(res, "not an admin");
+      //@ts-expect-error
+      await deleteFromCloud(portal.coverPhoto?.url);
+
+      await jobPortalModel.findByIdAndDelete(portalId);
+    } catch (e) {
+      AppResponse.fail(res, e);
+    }
+  };
+
+  getAJobPortal = async (req: Request, res: Response) => {
+    const { portalId } = req.params;
+
+    try {
+      const portal = await jobPortalModel.findById(portalId).lean();
+
+      if (!portal) return AppResponse.notFound(res);
+
+      AppResponse.success(res, portal);
+    } catch (e) {
+      AppResponse.fail(res, e);
+    }
+  };
+
+  getAllJobPortals = (req: Request, res: Response) => {
+    let query;
+    jobPortalModel
+      .find(query || {})
+      .then((portal) => {
+        const {
+          search = "",
+          perPage = 10,
+          page = 1,
+          sortBy = "createdAt",
+          sortDesc = false,
+          jobPortalId = "",
+          select = "all",
+        } = req.query;
+
+        const querylowerd = search.toLowerCase();
+
+        const filteredData = portal.filter((item) => {
+          return (
+            // search
+            item._id.toString().includes(querylowerd) &&
+            // Filter
+            item._id.toString() ===
+              (jobPortalId.toString() || item._id.toString())
+          );
+        });
+
+        const sortedData = filteredData.sort(sortData.sortCompare(sortBy));
+        if (sortDesc === "true") {
+          sortedData.reverse();
+        }
+
+        const dataFinal = sortData.selectFields(sortedData, select);
+        AppResponse.success(
+          res,
+          sortData.paginateArray(dataFinal, perPage, page),
+          filteredData.length
+        );
+      })
+      .catch((e) => {
+        AppResponse.fail(res, e);
+      });
+  };
+}
+
+export default new JobCntrl();
---
+++ service/src/model/Job.Model.ts
@@ -0,0 +1,96 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IJob extends Document {
+  title: string;
+  description: string;
+  content: string;
+  file: object;
+  category: Array<string>;
+  validityPeriod: string;
+  poster: Schema.Types.ObjectId;
+  portal: Schema.Types.ObjectId;
+}
+
+export interface IJobPortal extends Document {
+  name: string;
+  mode: string;
+  admins: Array<any>;
+  description: string;
+  coverPhoto: object;
+  duration: string;
+}
+
+const portalSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  mode: {
+    type: String,
+    enum: ["one-time", "long-term"],
+  },
+
+  admins: [{ type: Schema.Types.ObjectId, ref: "User" }],
+
+  description: {
+    type: String,
+    required: false,
+  },
+
+  duration: {
+    type: String,
+    enum: ["once", "thrice", "long-term"],
+  },
+
+  coverPhoto: {
+    url: String,
+    publicId: String,
+  },
+});
+
+const jobSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 30,
+  },
+
+  description: {
+    type: String,
+    required: true,
+    min: 10,
+  },
+
+  content: {
+    type: String,
+  },
+
+  file: {
+    publicId: String,
+    url: String,
+    fileType: String,
+  },
+
+  category: [{ type: String, required: true }],
+
+  validityPeriod: {
+    type: String,
+    enum: ["one-week", "two-weeks", "three-weeks", "one-month"],
+    default: "one-week",
+  },
+
+  poster: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+
+  portal: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+});
+
+export const jobModel = model<IJob>("Job", jobSchema);
+export const jobPortalModel = model<IJobPortal>("Jobportal", portalSchema);
