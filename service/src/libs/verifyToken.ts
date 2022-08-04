8a770c2793c1d41fba11a97f5e5dda46c5c9fbb2
Andry Rasolofomanana <andry.rasolofomananaa@gmail.com>
2022-08-04 09:46:37 +0300

    fix: fix process env name

--- service/src/libs/verifyToken.ts
+++ service/src/libs/verifyToken.ts
@@ -23,7 +23,7 @@ class ValidationToken {
       if (!bearerToken) return res.status(401).json("Access Denied");
       const payload = jwt.verify(
         bearerToken,
-        process.env["SECRET_TOKEN"] || "defaultToken"
+        process.env.JWT_SECRET || "defaultToken"
       ) as IPayload;
 
       const user = await Users.findById(payload._id).select([
